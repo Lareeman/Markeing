fe93595110b0c2c511a87a7862143369ab6c48c8
Lareeman <127728745+Lareeman@users.noreply.github.com>
2023-03-14 10:49:05 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+  var mixer = mixitup('.portfolio__content');
+	$('.slider-blog__inner').slick({
+		dots: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
+	});
+});
