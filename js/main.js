7789a8c4ad8b5d94fc18de34fa3559c8d52a85ae
AleksF2022 <aleksfomin52@gmail.com>
2023-03-15 17:32:42 +0200

    header adaptive finished

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-$(function(){
-  var mixer = mixitup('.portfolio__content');
+$(function () {
+	var mixer = mixitup('.portfolio__content');
 	$('.slider-blog__inner').slick({
 		dots: true,
 		prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
 	});
-	
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').toggleClass('menu__list--active');
+		$('.header__top-inner').toggleClass('header__top-inner--active');
+	});
 });
