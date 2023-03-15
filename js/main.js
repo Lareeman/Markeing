5512047fdd6be749f154b567ff8dd4340ce8219d
AleksF2022 <aleksfomin52@gmail.com>
2023-03-15 10:16:46 +0200

    media не закінчео

--- js/main.js
+++ js/main.js
@@ -5,4 +5,5 @@ $(function(){
 		prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
 	});
+	
 });
